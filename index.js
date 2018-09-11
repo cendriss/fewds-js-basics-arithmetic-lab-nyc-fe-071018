2c607172d708d732e947116433181dd3e2a37945
Christine Endriss <cendriss@gmail.com>
2018-09-11 22:12:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ let newID = oldID + 1000000000;
 
 let ageIsValid = Number.isInteger(currentAge);
 
-let randomNumber = Math.floor(Math.random() * 20) + 0
+let randomNumber = Math.floor(Math.random() * 20) + 0;
+
+let randomInteger = randomNumber
