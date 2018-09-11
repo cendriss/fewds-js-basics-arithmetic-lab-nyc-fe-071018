ef811ec25e6eee56db168045817e34d046941524
Christine Endriss <cendriss@gmail.com>
2018-09-11 22:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ Variables defined in the test suite:
 
 You should NOT try to redefine any of the above variables.
 */
-const newID = 1000000001
+
 
 
 // Write your code below this line!
+const newID = 1000000001
